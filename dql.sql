e2e821a5b638981c13882a437a7903fb6c3efcb3
Anu Maryjohn <113846480+Maryjohnalao@users.noreply.github.com>
2022-11-17 12:53:03 +0100

    update dql.sql

--- dql.sql
+++ dql.sql
@@ -5,7 +5,7 @@ SELECT * FROM product ORDER BY price DESC;
 SELECT SUM(quantity), AVG(total_amount), MAX(total_amount), MIN(total_amount) FROM orders;
 SELECT DISTINCT product_id, quantity FROM orders;
 SELECT customer_id FROM orders WHERE quantity > 2;
----select count(customer_id), extract (month from order_date) as month from orders group by order_date
+SELECT EXTRACT(MONTH FROM order_date), quantity from orders;
 SELECT product_name, customer_name, order_date FROM orders inner JOIN product ON orders.product_id = product.product_id JOIN customer ON orders.customer_id = customer.customer_id
 SELECT * FROM orders WHERE order_date > current_date - interval '3 months'
 SELECT customer_name FROM Customer LEFT JOIN Orders ON customer.customer_id = orders.Customer_id WHERE orders.Customer_id IS NULL
