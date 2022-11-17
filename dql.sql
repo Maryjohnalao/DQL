c084d670053f7184d2972b01d8ccacc74b5c00df
Anu Maryjohn <113846480+Maryjohnalao@users.noreply.github.com>
2022-11-17 12:29:18 +0100

    UPDATE DQL.SQL

--- dql.sql
+++ dql.sql
@@ -1,11 +1,11 @@
  ---dql
-select * from Customer;
-select product_name, category from product where price between 5000 and 10000;
-select * from product order by price DESC;
-select sum(quantity), avg(total_amount), max(total_amount), min(total_amount)from orders;
-select distinct product_id, quantity from orders;
-select customer_id from orders where quantity > 2;
----select count(customer_id), extract (month from order_date) as 'month' from orders group by order_date
----select product.product_name, customer.customer_name, order_date from orders INNER JOIN product ON orders.product_id INNER JOIN customer ON orders.customer_id = customer.customer_id
-select * from orders where order_date > current_date - interval '3 months'
-select customer_name FROM Customer LEFT JOIN Orders ON customer.customer_id = orders.Customer_id WHERE orders.Customer_id IS NULL
+SELECT * FROM Customer;
+SELECT product_name, category FROM product WHERE price BETWEEN 5000 AND 10000;
+SELECT * FROM product ORDER BY price DESC;
+SELECT SUM(quantity), AVG(total_amount), MAX(total_amount), MIN(total_amount) FROM orders;
+SELECT DISTINCT product_id, quantity FROM orders;
+SELECT customer_id FROM orders WHERE quantity > 2;
+---select count(customer_id), extract (month from order_date) as month from orders group by order_date
+SELECT product_name, customer_name, order_date FROM orders inner JOIN product ON orders.product_id = product.product_id JOIN customer ON orders.customer_id = customer.customer_id
+SELECT * FROM orders WHERE order_date > current_date - interval '3 months'
+SELECT customer_name FROM Customer LEFT JOIN Orders ON customer.customer_id = orders.Customer_id WHERE orders.Customer_id IS NULL
