2d3e4f19aa811dc7eb45cff7d7d72b976ca8625e
Anu Maryjohn <113846480+Maryjohnalao@users.noreply.github.com>
2022-12-01 14:14:01 +0100

    Update dql.sql

--- dql.sql
+++ dql.sql
@@ -3,7 +3,7 @@ SELECT * FROM Customer;
 SELECT product_name, category FROM product WHERE price BETWEEN 5000 AND 10000;
 SELECT * FROM product ORDER BY price DESC;
 SELECT SUM(quantity), AVG(total_amount), MAX(total_amount), MIN(total_amount) FROM orders;
-SELECT DISTINCT product_id, quantity FROM orders;
+SELECT product_id, SUM(quantity) FROM orders GROUP BY product_id;
 SELECT customer_id FROM orders WHERE quantity > 2;
 SELECT EXTRACT(MONTH FROM order_date) AS month, extract(year FROM order_date) AS year, COUNT(order_id) fROM orders ORDER BY month WHERE year = '2020'; 
 SELECT product_name, customer_name, order_date FROM orders inner JOIN product ON orders.product_id = product.product_id JOIN customer ON orders.customer_id = customer.customer_id
